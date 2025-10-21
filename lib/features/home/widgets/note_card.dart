5a802c01147fdba460aad343aab9bb69ea33dcf9
Maya Alhoussein <131613264+Maya-Alhoussein@users.noreply.github.com>
2025-10-21 13:59:44 +0300

    feat(noteDetails): add note details screen

--- lib/features/home/widgets/note_card.dart
+++ lib/features/home/widgets/note_card.dart
@@ -1,5 +1,6 @@
 import 'package:note_app_final/common_imports.dart';
 import 'package:note_app_final/data/models/note/note.dart';
+import 'package:note_app_final/features/note_details/note_details_screen.dart';
 
 class NoteCard extends StatelessWidget {
   final Note note;
@@ -8,38 +9,60 @@ class NoteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: note.color,
-        borderRadius: BorderRadius.circular(15.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            transitionDuration: const Duration(milliseconds: 400),
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                NoteDetailsScreen(note: note),
+            transitionsBuilder: (context, animation, secondaryAnimation, child) {
+              final curvedAnimation = CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeInOutCubic,
+              );
+
+              return FadeTransition(
+                opacity: curvedAnimation,
+                child: child,
+              );
+            },
           ),
-        ],
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomText(
-            text: note.title,
-            fontSize: 20.sp,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-          SizedBox(height: 8.h),
-          Expanded(
-            child: Text(
-              note.content,
-              style:  TextStyle(fontSize: 16.sp, color: Colors.black38),
-              overflow: TextOverflow.fade,
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: note.color,
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.05),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 3),
             ),
-          ),
-        ],
+          ],
+        ),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(
+              text: note.title,
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            SizedBox(height: 8.h),
+            Expanded(
+              child: Text(
+                note.content,
+                style:  TextStyle(fontSize: 16.sp, color: Colors.black38),
+                overflow: TextOverflow.fade,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/note_details/note_details_screen.dart
+++ lib/features/note_details/note_details_screen.dart
@@ -1,10 +1,23 @@
 import 'package:note_app_final/common_imports.dart';
+import 'package:note_app_final/data/models/note/note.dart';
 
 class NoteDetailsScreen extends StatelessWidget {
-  const NoteDetailsScreen({super.key});
+  final Note note;
+  const NoteDetailsScreen({super.key, required this.note});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: note.color,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: CustomText(text : note.title,fontSize: 20.sp),
+      ),
+      body: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: 20.w,vertical: 16.h),
+        child: SingleChildScrollView(child: CustomText(text: note.content,alignment: TextAlign.center,fontSize: 16.sp,overflow: TextOverflow.visible,)),
+      ),
+    );
   }
 }
